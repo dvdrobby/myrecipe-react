b0db97637c975ad995bfd2fca506c71dfa1d1143
dvdrobby <david.robby.sanjaya@gmail.com>
2025-03-17 22:59:56 +0700

    initial commit and configure react-router-dom

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Browse from "./pages/Browse";
+
+
+function App() {
+  return (
+   <Router>
+    <Routes>
+      <Route path="/" element={<Browse/>}/>
+    </Routes>
+   </Router>
+  )
+}
+
+export default App
